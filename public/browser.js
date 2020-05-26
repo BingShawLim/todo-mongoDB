f621affb97efa9889e99a92a190a8535fcbdcce9
BingShawLim <shaw.a.muvee@gmail.com>
2020-05-25 21:41:34 -0500

    rearranged create new item function.

--- public/browser.js
+++ public/browser.js
@@ -1,3 +1,26 @@
+function itemTemplate(item) {
+    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+      <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+      <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+  </li>`
+}
+//create
+let createFeild = document.getElementById("create-field")
+document.getElementById("create-form").addEventListener("submit", (e)=>{
+    e.preventDefault()
+    axios.post('/create-item', {text: createFeild.value})
+            .then((response)=>{
+                document.getElementById("item-list").insertAdjacentHTML("beforeend",itemTemplate(response.data))
+                createFeild.value = ""
+                createFeild.focus()
+            .catch(()=>{
+                console.log("something wrong.")
+            }) 
+})
+
 document.addEventListener("click", (e) => {
     //delete
     if (e.target.classList.contains("delete-me")) {
